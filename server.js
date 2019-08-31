8d1b1eea6d21cb6f3c983d2c0b1ac815bccff18e
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2019-08-31 10:06:09 +0200

    Initial setup

---
+++ server.js
@@ -0,0 +1,33 @@
+    
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load()
+  }
+
+
+const express= require('express');
+const app=express();
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter=require('./routes/index')
+
+app.set('view engine', 'ejs');
+app.set('views',__dirname +'/views')
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+const mongoose=require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error',()=>{
+    console.log('connection error123')
+  } );
+  db.once('open',  ()=> {
+    console.log('succes connected Mongoose');
+  });
+
+app.use('/', indexRouter)
+
+
+app.listen(process.env.PORT||3000)
