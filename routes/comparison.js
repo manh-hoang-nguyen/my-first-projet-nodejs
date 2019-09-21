aa8a5c29b8974a00d27188e7e22dc66bcb8154ec
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2019-09-21 16:06:35 +0200

    add route get comparison

--- routes/comparison.js
+++ routes/comparison.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const comparisonController=require('../controllers/comparison');
 
-
+router.get('/',comparisonController.get);
 router.post('/',comparisonController.post);
 router.delete('/',comparisonController.delete);
 
